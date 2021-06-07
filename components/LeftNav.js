cb388a82a990ddf2e495427b29867e280915d747
Javier Alaves <jayalaves@gmail.com>
2021-06-07 11:32:16 +0200

    Add hover to left nav items

--- components/LeftNav.js
+++ components/LeftNav.js
@@ -14,12 +14,17 @@ const MenuItem = styled.div`
     height: 40px;
     border-radius: 6px;
     padding-left: 1rem;
-    margin-bottom: 0.25rem;
+    margin-bottom: 0.125rem;
     align-items: center;
     font-size: 14px;
     font-weight: ${props => props.active ?  "500" : "400"};
     background-color: ${props => props.active ?  "#E0E7FF" : "white"};
     color: ${props => props.active ?  "#4F46E5" : "black"};
+    cursor: pointer;
+
+    &:hover {
+        background-color: #E0E7FF;
+    }
 `;
 
 const LeftNav = () => {
